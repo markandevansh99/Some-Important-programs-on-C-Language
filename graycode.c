fd86d3fdb16340797453f07bd88671fe93e70193
Devansh Markan <46858847+markandevansh99@users.noreply.github.com>
2020-01-21 16:41:19 +0530

    Convert Binary no. into Gray code

---
+++ graycode.c
@@ -0,0 +1,27 @@
+// Conversion of a binary number into Gray Code
+
+#include <stdio.h>
+#include <math.h>
+#include<stdlib.h>
+ 
+ 
+// This is the function which converts into gray code
+int grayconversion(int binary)
+{
+    int n1, n2;
+    int answer = 0, counter = 0;
+
+    while (binary != 0)
+    {
+        n1 = binary % 10;
+        binary = binary / 10;
+        n2 = binary % 10;
+        if ((n1 && !n2) || (!n1 && n2))
+        {
+            answer = answer + pow(10, counter);
+        }
+        counter++;
+    }
+    return answer;
+}
+
