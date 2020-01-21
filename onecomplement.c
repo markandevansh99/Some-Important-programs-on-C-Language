e35531f400e004351b382063f0e59a76a7d711ad
Devansh Markan <46858847+markandevansh99@users.noreply.github.com>
2020-01-21 16:48:01 +0530

    Convert Binary number into its 1's Complement

---
+++ onecomplement.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int size=5;
+
+
+     int i, flag=0;
+     char bin[size + 1], ones_Comp[size + 1];
+
+
+    printf("Enter %d bit binary value: ", size);
+
+    gets(bin);
+
+   printf("\n");
