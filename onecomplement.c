2c457bb3ea3ccb189fe7f938ea6cbd9d47f38903
Devansh Markan <46858847+markandevansh99@users.noreply.github.com>
2020-01-21 16:51:27 +0530

    Conversion of Binary number into it's onecomplement.

--- onecomplement.c
+++ onecomplement.c
@@ -15,3 +15,35 @@ int main()
     gets(bin);
 
    printf("\n");
+    
+  // This is the for loop which is responsible for the required conversion  
+  for(i=0; i<size; i++)
+    {
+        if(bin[i] == '1')
+        {
+            ones_Comp[i] = '0';
+        }
+        else if(bin[i] == '0')
+        {
+            ones_Comp[i] = '1';
+        }
+        else
+        {
+            printf("Enter only 5 size input");
+            flag = 1;
+            break;
+        }
+    }
+
+    ones_Comp[size] = '\0';     // null value at the end. It is important
+
+    if(flag == 0)
+    {
+        printf("The binary number input by user := %s\n\n", bin);
+        printf("Ones complement of that binary number := %s", ones_Comp);
+        printf("\n");
+    }
+
+    return 0;
+}
+
